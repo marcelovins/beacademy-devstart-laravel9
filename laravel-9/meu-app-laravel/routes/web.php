c67770c38f72c9027425dc5cb04a1d2a6851039c
marcelovins <97124032+marcelovins@users.noreply.github.com>
2022-07-08 15:29:03 -0300

    feat: edit and update method
    in addition, it was created controllers and views

--- laravel-9/meu-app-laravel/routes/web.php
+++ laravel-9/meu-app-laravel/routes/web.php
@@ -31,6 +31,8 @@ Route::get('/', function () {
 
 // php artisan make:controller NomeDoController  (para criar controllers)
 
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
