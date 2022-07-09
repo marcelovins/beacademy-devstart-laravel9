a463078c874b95ed3d0e1cb4cd5c14035e8f9ed1
marcelovins <97124032+marcelovins@users.noreply.github.com>
2022-07-08 21:18:58 -0300

    feat: delete
    trying to do work first part of delete button. it doesnt work until that.

--- laravel-9/meu-app-laravel/routes/web.php
+++ laravel-9/meu-app-laravel/routes/web.php
@@ -31,12 +31,14 @@ Route::get('/', function () {
 
 // php artisan make:controller NomeDoController  (para criar controllers)
 
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
 
 
 //Via Cep Web Service
