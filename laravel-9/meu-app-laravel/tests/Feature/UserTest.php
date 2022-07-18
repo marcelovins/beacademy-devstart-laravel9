87683b9502a8759980e0663d0ccf42b671a69465
marcelovins <97124032+marcelovins@users.noreply.github.com>
2022-07-18 11:00:53 -0300

    feat: tests

---
+++ laravel-9/meu-app-laravel/tests/Feature/UserTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_user()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $this->actingAs($user);
+        
+        $response = $this->get('/users');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_create_user()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->post('/login/create', [
+            'name' => 'Peroba Oliveira',
+            'email' => 'peroba@perobinha.com',
+            'password' => '123456'
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
